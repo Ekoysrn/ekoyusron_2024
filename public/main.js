e723ed2b4eb56f50200261b33911719ea2d04ca6
Ekoysrn <ekoyusronardiansyah@gmail.com>
2024-10-05 06:32:32 +0700

    fixed bug

---
+++ public/main.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector("#menu");
+
+window.onscroll = function(){
+  const header = document.querySelector("header")
+  const fixedNav = header.offsetTop;
+
+  if(window.scrollY > fixedNav){
+    header.classList.add("nav-fixed");
+    header.classList.remove("absolute");
+  }else{
+    header.classList.remove("nav-fixed");
+  }
+}
+
+
+function hamburger(e){
+  e.classList.toggle("hamburger-active");
+  menu.classList.toggle("hidden")
+}

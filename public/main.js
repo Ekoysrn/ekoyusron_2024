726249351081a8dde3cb292a072240d21b1e1760
Ekoysrn <ekoyusronardiansyah@gmail.com>
2024-10-10 08:29:53 +0700

    improvement

--- public/main.js
+++ public/main.js
@@ -2,16 +2,16 @@
 
 // scrooling on navbar
 window.onscroll = function(){
-  const header = document.querySelector("header")
+  const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
 
   if(window.scrollY > fixedNav){
     header.classList.add("nav-fixed");
-    header.classList.remove("absolute");
   }else{
     header.classList.remove("nav-fixed");
   }
 }
+
 // target hamburger
 const btn = document.querySelector("#btn");
 const menu = document.querySelector("#menu");
@@ -32,23 +32,38 @@ function hamburger(e){
 // })
 
 // target darkMode Toggle
-const darkToggle = document.querySelector("#darkToggle");
-const html = document.documentElement;
-const darkToggleSm = document.querySelector("#darkToggleSm");
-const github = document.querySelector("#github");
+const darkToggle = document.querySelector("#darkToggle"),
+      html = document.documentElement,
+      darkToggleSm = document.querySelector("#darkToggleSm"),
+      github = document.querySelector("#github"),
+      badgeImg = document.querySelectorAll(".tech");
+
+
+// techBadge
+function techBadge(theme){
+  badgeImg.forEach((img) => {
+    if (img.src.includes("?theme=")) {
+      img.src = img.src.replace(/(theme=)(light|dark)/, `$1${theme}`);
+    }
+  });
+}
 
 // darkMode
 function toggleTheme(isDark){
-  html.dataset.theme = isDark ? "dark" : "light";
+  let theme = html.dataset.theme = isDark ? "dark" : "light";
   darkToggle.checked = isDark;
   darkToggleSm.checked = isDark;
+
+  
   
   if(isDark){
     github.setAttribute("fill","#fff");
-    localStorage.theme = "dark"
+    localStorage.theme = "dark";
+    techBadge(theme)
   }else{
     github.removeAttribute("fill");
-    localStorage.theme = "light"
+    localStorage.theme = "light";
+    techBadge(theme)
   };
 }
 
@@ -65,9 +80,11 @@ darkToggleSm.addEventListener("click",()=>{
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   darkToggle.checked = true;
   darkToggleSm.checked = true;
+  techBadge(theme)
 } else {
   darkToggle.checked = false;
   darkToggleSm.checked = false;
+  techBadge(theme)
 };
 
 //alert coming soon 
